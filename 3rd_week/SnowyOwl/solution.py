c6faf221bf3010378bffcd8a4c92d5afbbe69697
SnovvyOwl <owl0614@naver.com>
2023-09-19 22:49:45 +0900

    E

--- 3rd_week/SnowyOwl/solution.py
+++ 3rd_week/SnowyOwl/solution.py
@@ -1,3 +1,39 @@
-def solution(nums):
-    answer = 0
+# def solution(n, times):
+#     min_time,max_time=0, max(times)*n
+#     while min_time<=max_time:
+#         mid=(min_time+max_time)//2 # 중간 시간 찾기
+#         people=0
+#         for time in times:
+#             people+=mid//time # 중간시간에 찾은 사람명수 구하기
+#             if people>=n:
+#                 break
+#         if people>=n: #다함 
+#             answer=mid
+#             max_time=mid-1
+#         elif people<n: #아직 다못함
+#             min_time=mid+1
+    
+#     return answer
+
+
+
+
+def solution(people, limit):
+    people.sort()
+    answer=0
+    shiped=0
+    for p in people:
+        shiped+=p
+        if shiped>limit:
+            answer+=1
+            shiped=p
+        elif shiped==limit:
+            answer+=1
+            shiped=0
+        elif p==people[-1] and shiped:
+            answer+=1
     return answer
+
+
+if __name__=="__main__":
+    print(solution([50,70,80,50],100))
